777803fcb5fa91f354855f1b3fac7d7bbeeea250
Anders Wallin <touch.dev.null@gmail.com>
2018-03-04 00:05:45 +0100

    Added html and CORS to the response.

--- main.go
+++ main.go
@@ -25,7 +25,10 @@ type MusslanBeerTime struct {
 
 var (
 	headers = map[string]string{
-		"content-type": "application/json",
+		"Content-Type":                 "application/json",
+		"Access-Control-Allow-Origin":  "*",
+		"Access-Control-Allow-Methods": "GET,OPTIONS",
+		"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
 	}
 )
 
