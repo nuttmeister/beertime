3f2468323bc5b103f1dd6cb82e3eb055a8e50258
Anders Wallin <touch.dev.null@gmail.com>
2022-10-10 22:33:21 +0700

    Remove universal beer constant.

--- beertime_test.go
+++ beertime_test.go
@@ -28,27 +28,22 @@ func TestBeerTime(t *testing.T) {
 	assert.Equal(t, false, Now(date))
 	assert.Equal(t, float64(4*24+4), Duration(date).Hours())
 
-	// Test for beertime on saturday 00:00. Right week directly after beertime.
-	date, _ = time.Parse(dateFormat, "2020-11-21 00:00")
-	assert.Equal(t, false, Now(date))
-	assert.Equal(t, float64(14*24-9), Duration(date).Hours())
-
-	// Test for beertime on saturday 15:00. Right week directly after beertime.
+	// Test for beertime on saturday 15:00.
 	date, _ = time.Parse(dateFormat, "2020-11-21 15:00")
 	assert.Equal(t, false, Now(date))
-	assert.Equal(t, float64(13*24), Duration(date).Hours())
+	assert.Equal(t, float64(6*24), Duration(date).Hours())
 
-	// Test for beertime on thursday 15:00. But wrong week.
+	// Test for beertime on thursday 15:00.
 	date, _ = time.Parse(dateFormat, "2020-11-12 15:00")
 	assert.Equal(t, false, Now(date))
-	assert.Equal(t, float64(8*24), Duration(date).Hours())
+	assert.Equal(t, float64(1*24), Duration(date).Hours())
 
-	// Test for beertime on friday 15:00. But wrong week.
+	// Test for beertime on friday 15:00.
 	date, _ = time.Parse(dateFormat, "2020-11-13 15:00")
-	assert.Equal(t, false, Now(date))
-	assert.Equal(t, float64(7*24), Duration(date).Hours())
+	assert.Equal(t, true, Now(date))
+	assert.Equal(t, float64(0), Duration(date).Hours())
 
-	// Test for beertime on saturday 00:00. But wrong week.
+	// Test for beertime on saturday 00:00.
 	date, _ = time.Parse(dateFormat, "2020-11-14 00:00")
 	assert.Equal(t, false, Now(date))
 	assert.Equal(t, float64(6*24+15), Duration(date).Hours())
